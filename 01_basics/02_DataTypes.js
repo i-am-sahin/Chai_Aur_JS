67e26ff4242c356e9c73e0ffbbe25b53d71df173
Sahin Islam <sahinislam878@gmail.com>
2024-07-14 12:00:59 +0530

    02_DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //It's work in Browser,&& here in local machine we're using node js 
+
+console.log(3+3)
+console.log("sahin")
+
+let name = "Sahin"
+let age = 18
+let isLoggedIn = false
+
+// number => ~2^53
+// bigint
+// String
+// boolean => true/false
+// null => standAlone Value/type.....It is an Object
+// undefined => not defined yet! ex: let state;
+// symbol => Find Uniqueness
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
