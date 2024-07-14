9c67b5e34d4db64b1e479ae1b8dea8ada0fc8e2e
Sahin Islam <sahinislam878@gmail.com>
2024-07-14 13:09:03 +0530

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "true"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33asd" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = "Sahin"
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log("The value of boolIsLoggedIn is : ",boolIsLoggedIn ,"&& type of boolIsLoggedIn is : " , typeof boolIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false;
+// "sahin" => true
+
+let someString = 33
+let strNumber = String(someString);
+
+
+console.log(someString);
+console.log(typeof strNumber);
